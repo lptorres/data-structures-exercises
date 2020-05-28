1e2b5c651f796cde0415daad9bcb33ee03f91ce3
Ton Torres <ton.torres@outlook.com>
2020-05-28 15:07:26 +0800

    [CHAPTER1] Implemented custom Linked List class

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -4,7 +4,7 @@ Chapter 1:
 """
 
 
-def gcd(m, n):
+def gcd(m: int, n: int):
     """Euclid's algorithm for finding the greatest common divisor (GCD) of two
     positive integers _m_ and _n_.
     """
@@ -17,9 +17,127 @@ def gcd(m, n):
     return n
 
 
-def gcd_recursive(m, n):
+def gcd2(m: int, n: int):
+    """Euclid's algorithm using Python3.8's walrus assignment"""
+    while (r := m % n) != 0:
+        m = n
+        n = r
+    return n
+
+
+def gcd_recursive(m: int, n: int):
     """Recursive implementation of Euclid's algorithm"""
     r = m % n
     if r == 0:
         return n
     return gcd_recursive(m=n, n=r)
+
+
+class Node:
+    """Class representing an element in a linked list"""
+    def __init__(self, info: int, link=None):
+        """Initialize a Node.
+
+        Keyword arguments:
+        info -- the int data on this node
+        link -- the next Node (if any) in the list
+        """
+        if link:
+            assert type(link) == type(self)
+        self.info = info
+        self.link = link
+
+    def __str__(self):
+        """String representation of an instance"""
+        return str(self.info)
+
+    def set_link(self, link=None):
+        """Set the value of self.link"""
+        if link:
+            assert type(link) == type(self)
+        self.link = link
+
+
+class LinkedList:
+    """Class representing a linked list"""
+    __head = None
+    __tail = None
+    __count = 0
+
+    def __init__(self, items: list = []):
+        """Initialize a linked list.
+
+        Keyword arguments:
+        items - a list containing the elements
+        """
+        for item in items:
+            self.append(item)
+
+    def __str__(self):
+        """Return the string representation of the linked list"""
+        cur = self.__head
+        items = []
+        while True:
+            items.append(cur.info)
+            if cur.link:
+                cur = cur.link
+            else:
+                break
+        return str(items)
+
+    def __len__(self):
+        """Return the length of the linked list"""
+        return self.__count
+
+    def __getitem__(self, index: int):
+        """Return the element at the selected index"""
+        if index >= self.__count:
+            raise IndexError
+        cur = self.__head
+        for _ in range(index):
+            cur = cur.link
+        return cur.info
+
+    def head(self):
+        """Return the head of the linked list"""
+        return self.__head
+
+    def tail(self):
+        """Return the tail of the linked list"""
+        return self.__tail
+
+    def append(self, info: int):
+        """Insert an element at the end of the list"""
+        node = Node(info)
+        if not self.__head:
+            self.__head = node
+        else:
+            cur = self.__head
+            while cur.link:
+                cur = cur.link
+            cur.set_link(node)
+        self.__tail = node
+        self.__count += 1
+
+    def prepend(self, info: int):
+        """Insert an element at the start of the list"""
+        node = Node(info)
+        if not self.__head:
+            self.__head = node
+        else:
+            node.set_link(self.__head)
+            self.__head = node
+        self.__count += 1
+
+    def insert_sorted(self, info: int):
+        """Insert an element into its sorted position in the list"""
+        if not self.__head:
+            self.append(info)
+            return
+        if info < self.__head.info:
+            self.__head = Node(info, link=self.__head)
+        else:
+            cur = self.__head
+            while cur.link and info > cur.link.info:
+                cur = cur.link
+            cur.link = Node(info, link=cur.link)
--- chapter1/tests.py
+++ chapter1/tests.py
@@ -1,11 +1,11 @@
 """
-Test class for chapter 1
+Tests for chapter 1
 """
 from math import gcd as pygcd
 from random import randint
 import unittest
 
-from chapter1 import gcd, gcd_recursive
+from chapter1 import gcd, gcd2, gcd_recursive, LinkedList
 
 
 class TestGCD(unittest.TestCase):
@@ -34,6 +34,32 @@ class TestGCD(unittest.TestCase):
         self.assertEqual(GCD, pygcd(m, n))
 
 
+class TestGCD2(unittest.TestCase):
+    """Test the `chapter1.gcd2` function"""
+
+    def test_gcd_book_example(self):
+        """Test the gcd function using the example from the book:
+        `m` = 608
+        `n` = 133
+        """
+        GCD = gcd2(608, 133)
+        self.assertEqual(GCD, 19)
+
+    def test_gcd_random_small(self):
+        """Test the gcd function using random integers until 1000."""
+        m = randint(1, 1000)
+        n = randint(1, 1000)
+        GCD = gcd2(m, n)
+        self.assertEqual(GCD, pygcd(m, n))
+
+    def test_gcd_random_large(self):
+        """Test the gcd function using random integers until 1 billion."""
+        m = randint(1, 1000000000)
+        n = randint(1, 1000000000)
+        GCD = gcd2(m, n)
+        self.assertEqual(GCD, pygcd(m, n))
+
+
 class TestGCDRecursive(unittest.TestCase):
     """Test the `chapter1.gcd_recursive` function"""
 
@@ -58,6 +84,66 @@ class TestGCDRecursive(unittest.TestCase):
         n = randint(1, 1000000000)
         GCD = gcd_recursive(m, n)
         self.assertEqual(GCD, pygcd(m, n))
+
+
+class TestLinkedList(unittest.TestCase):
+    """Test the `chapter1.LinkedList` class"""
+    def setUp(self):
+        """Initialize a random integer generator"""
+        self.random_ints = (randint(1, 1000000000) for _ in range(1, 1000))
+
+    def test_prepend(self):
+        """Test prepending"""
+        my_list = LinkedList()
+        python_list = []
+        for i in self.random_ints:
+            my_list.prepend(i)
+            python_list.insert(0, i)
+            self.assertEqual(my_list.head().info, python_list[0])
+
+    def test_append(self):
+        """Test appending"""
+        my_list = LinkedList()
+        python_list = []
+        for i in self.random_ints:
+            my_list.append(i)
+            python_list.append(i)
+            self.assertEqual(my_list.tail().info, python_list[-1])
+
+    def test_len(self):
+        """Test the length of the list"""
+        my_list = LinkedList()
+        python_list = []
+        for i in self.random_ints:
+            my_list.append(i)
+            python_list.append(i)
+            self.assertEqual(len(my_list), len(python_list))
+
+    def test_elements(self):
+        """Test the elements of the list"""
+        python_list = [_ for _ in self.random_ints]
+        my_list = LinkedList(python_list)
+        cur = my_list.head()
+        for item in python_list:
+            self.assertEqual(cur.info, item)
+            cur = cur.link
+
+    def test_getitem(self):
+        """Test retrieval of individual items of the list"""
+        python_list = [_ for _ in self.random_ints]
+        my_list = LinkedList(python_list)
+        for i in range(len(my_list)):
+            self.assertEqual(my_list[i], python_list[i])
+
+    def test_sort(self):
+        """Test sorting on the list"""
+        python_list = []
+        my_list = LinkedList()
+        for i in self.random_ints:
+            python_list.append(i)
+            python_list.sort()
+            my_list.insert_sorted(i)
+            self.assertEqual(str(my_list), str(python_list))
 
 
 if __name__ == '__main__':
