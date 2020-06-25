9059a1c641c42cd0999e1e9cae2e157e6e27c073
Ton Torres <ton.torres@outlook.com>
2020-06-25 16:26:36 +0800

    [CHAPTER5] Implemented topological sort

--- chapter5/chapter5.py
+++ chapter5/chapter5.py
@@ -4,6 +4,7 @@ Chapter 5:
     - Deque implementation (using a linked list)
     - Topological sort
 """
+from collections import defaultdict
 
 
 class Node:
@@ -115,3 +116,43 @@ class Queue:
         self.front = self.front.link
         self.__count -= 1
         return _front
+
+
+def topological_sort(pairs_in: tuple):
+    """Topological sort
+
+    Keyword arguments:
+    pairs_in -- iterable of tuples defining the partial ordering.
+                The first item in the tuple represents an item, and the second
+                item in the tuple represents the direct descendant.
+    """
+    def _build_partial_ordering():
+        """Helper function to build the partial ordering as a dict"""
+        out = defaultdict(lambda: {'count': 0, 'list': []})
+        for node, descendant in pairs_in:
+            out[node]['list'].insert(0, descendant)
+            out[descendant]['count'] += 1
+        return out
+
+    # build the partial ordering
+    partial_ordering = _build_partial_ordering()
+    # initialize the output queue
+    output = Queue()
+
+    # Guard against circular dependencies
+    if all([o['count'] > 0 for o in partial_ordering.values()]):
+        return
+
+    # Put initial (no predecessors) items into the output queue
+    for k, info in partial_ordering.items():
+        if info['count'] == 0:
+            output.enqueue(k)
+
+    while len(output) > 0:
+        item = output.dequeue()
+        descendants = partial_ordering[item.info]['list']
+        print(item, end=' ')
+        for desc in descendants:
+            partial_ordering[desc]['count'] -= 1
+            if partial_ordering[desc]['count'] == 0:
+                output.enqueue(desc)
